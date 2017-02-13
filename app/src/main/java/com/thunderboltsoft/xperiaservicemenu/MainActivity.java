bcb40cdfb01a7d72c0adc74c8a7c23bf9a0f1ece
kaozgamer <ktdp11195@yahoo.com>
2017-02-13 16:17:33 +0800

    Implemented fall back phone dialer method of opening service menu.

--- app/src/main/java/com/thunderboltsoft/xperiaservicemenu/MainActivity.java
+++ app/src/main/java/com/thunderboltsoft/xperiaservicemenu/MainActivity.java
@@ -8,6 +8,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
@@ -18,15 +21,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b;
+    Button btnOpenServiceMenu, btnManuallyOpenServiceMenu;
     SharedPreferences prefs;
     Boolean bShowInfoDialog;
+    Intent intentOpenServiceMenu;
+    Boolean bServiceMenuExists;
+    TextView txtError, txtDisclaimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +43,52 @@ public class MainActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
         }
 
-        b = (Button) findViewById(R.id.button1);
-
-
+        btnOpenServiceMenu = (Button) findViewById(R.id.btnOpenServiceMenu);
+        btnManuallyOpenServiceMenu = (Button) findViewById(R.id.btnManuallyOpenServiceMenu);
 
         createListeners();
 
+        TextView txtDeviceInfoName = (TextView) findViewById(R.id.txtDeviceInfoName);
+        txtError = (TextView) findViewById(R.id.error);
+        txtDisclaimer = (TextView) findViewById(R.id.disclaimer_text);
+        String deviceInfoName = getDeviceName();
+
+        if (deviceInfoName.equals("")) {
+            txtDeviceInfoName.setText(R.string.device_info_name_error);
+        } else {
+            txtDeviceInfoName.setText(getDeviceName());
+        }
+
+        bServiceMenuExists = setupStartServiceMenu();
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         bShowInfoDialog = prefs.getBoolean("showDialog", true);
 
         if (bShowInfoDialog) {
             showInfoDialog();
+        }
+    }
+
+    public String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return " " + capitalize(model);
+        } else {
+            return " " + capitalize(manufacturer) + " " + model;
+        }
+    }
+
+
+    private String capitalize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
         }
     }
 
@@ -118,46 +158,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createListeners() {
-
-        b.setOnClickListener(new OnClickListener() {
+        btnOpenServiceMenu.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 startServiceMenu();
             }
         });
+
+        btnManuallyOpenServiceMenu.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "*#*#7378423#*#*", null)));
+            }
+        });
     }
 
-    private void startServiceMenu() {
-        Intent intent = new Intent("android.intent.action.MAIN");
-        intent.setComponent(ComponentName.unflattenFromString("com.sonyericsson.android.servicemenu/com.sonyericsson.android.servicemenu.ServiceMainMenu"));
-        intent.addCategory("android.intent.category.LAUNCHER");
+    private boolean setupStartServiceMenu() {
+        intentOpenServiceMenu = new Intent("android.intent.action.MAIN");
+        intentOpenServiceMenu.setComponent(ComponentName.unflattenFromString("com.sonyericsson.android.servicemenu/com.sonyericsson.android.servicemenu.ServiceMainMenu"));
+
+        intentOpenServiceMenu.addCategory("android.intent.category.LAUNCHER");
 
         PackageManager packageManager = getPackageManager();
-        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(intentOpenServiceMenu, 0);
         boolean isIntentSafe = activities.size() > 0;
 
         if (isIntentSafe) {
-            startActivity(intent);
+            return true;
         } else {
-            Toast.makeText(this, "Unable to find the service menu app on this device", Toast.LENGTH_LONG).show();
+            txtError.setText("ERROR: Unable to find the service menu apk on this device.\n\nPossible explanations are:\n1. You are running a custom ROM\n2. You are attempting to run this app on a non Sony device\n3. You may have changed the device ID to a non Sony device ID inside build.prop\n\nAlternatively you can try to manually open the service menu by dialing the special code. Click on the following button to be redirected to the phone dialer with the code pre-entered.");
+            txtError.setTextColor(Color.RED);
+            btnOpenServiceMenu.setVisibility(View.GONE);
+            btnManuallyOpenServiceMenu.setVisibility(View.VISIBLE);
+            txtDisclaimer.setVisibility(View.GONE);
+            return false;
         }
     }
 
-    @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
-        super.onPause();
+    private void startServiceMenu() {
+        if (bServiceMenuExists) {
+            startActivity(intentOpenServiceMenu);
+        }
     }
-
-    @Override
-    protected void onResume() {
-        // TODO Auto-generated method stub
-        super.onResume();
-    }
-
-    @Override
-    protected void onStop() {
-        // TODO Auto-generated method stub
-        super.onStop();
-    }
-
 }
